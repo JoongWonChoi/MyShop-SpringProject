4b27488fa2e828cee154f87caa58ee322620bcea
JoongWonChoi <qweasd1130@naver.com>
2022-02-01 16:15:47 +0900

    Add CSS/Bootstrap for layout

---
+++ src/main/java/jwcbook/jwcshop/controller/HomeController.java
@@ -0,0 +1,18 @@
+package jwcbook.jwcshop.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j // lombokr의 logging 을 위한 어노테잉션
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
